183b31d27508a0ff211bb1f04df01e38910bcd4d
Paulo19961944 <106405308+Paulo19961944@users.noreply.github.com>
2023-07-06 10:29:40 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from datetime import date
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    data_nascimento = date(1996, 4, 24)  # Exemplo de data de nascimento
+    hoje = date.today()
+    idade = hoje.year - data_nascimento.year - ((hoje.month, hoje.day) < (data_nascimento.month, data_nascimento.day))
+    return render_template('index.html', idade=idade)
+
+if __name__ == '__main__':
+    app.run()
